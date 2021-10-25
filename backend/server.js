1e7b0cfc7e8228eda61b3158507a7392ec02fa78
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-10-24 18:58:15 -0700

    configuring db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
+
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server is listening on PORT 5000");
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`);
 });
