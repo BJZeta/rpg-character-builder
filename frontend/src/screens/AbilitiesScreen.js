6a520af45008550773e68f9704da145f389e4fb2
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-12-12 21:08:04 -0800

    removing redux, way too much for project

--- frontend/src/screens/AbilitiesScreen.js
+++ frontend/src/screens/AbilitiesScreen.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+// import { checkSelectedRace } from "../actions/characterActions";
+
 export default function AbilitiesScreen() {
+  useEffect(() => {
+    const checkRace = localStorage.getItem("race");
+
+    if (checkRace) {
+      console.log("selected race");
+    } else {
+      alert("No Race Selected");
+    }
+  }, []);
+
   return (
     <div>
       <h1>Abilities Screen</h1>
