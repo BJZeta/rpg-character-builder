0c5cecdefc300f7c5d946d881a614cd5c5d0bebd
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-12-13 19:07:13 -0800

    adding arrow buttons instead of navbar

--- frontend/src/screens/NameScreen.js
+++ frontend/src/screens/NameScreen.js
@@ -1,11 +1,35 @@
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill,
+} from "react-icons/bs";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 export default function NameScreen() {
   return (
-    <div>
+    <NameLayout>
+      <Link to="/">
+        <BsFillArrowLeftSquareFill className="arrow-button" />
+      </Link>
       <h1>Name Screen</h1>
       <section>
         <img src={`${process.env.PUBLIC_URL}/img/placeholder.png`} />
         <input />
       </section>
-    </div>
+      <Link to="/home/race">
+        <BsFillArrowRightSquareFill className="arrow-button" />
+      </Link>
+    </NameLayout>
   );
 }
+
+const NameLayout = styled.div`
+  .arrow-button {
+    font-size: 50px;
+    color: black;
+  }
+
+  .arrow-button:hover {
+    color: red;
+  }
+`;
--- frontend/src/screens/RaceScreen.js
+++ frontend/src/screens/RaceScreen.js
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { BsFillArrowLeftSquareFill, BsFillArrowRightSquareFill } from "react-icons/bs";
+import styled from "styled-components";
 import RaceCard from "../components/RaceCard";
 import dragonborn from "../assets/dragonborn.jpeg";
 import half_elf from "../assets/half-elf.jpeg";
@@ -9,16 +12,41 @@ import SubraceCard from "../components/SubraceCard";
 
 export default function RaceScreen() {
   return (
-    <div>
+    <RaceLayout>
+      <Link to="/home/name">
+        <BsFillArrowLeftSquareFill className="arrow-button" />
+      </Link>
       <h1>Race Screen</h1>
       <section>
-        <RaceCard selectedRace="dragonborn" img={dragonborn} icon={dragonborn} />
-        <SubraceCard selectedRace="elf" selectedSubrace="high-elf"  img={high_elf} />
+        <RaceCard
+          selectedRace="dragonborn"
+          img={dragonborn}
+          icon={dragonborn}
+        />
+        <SubraceCard
+          selectedRace="elf"
+          selectedSubrace="high-elf"
+          img={high_elf}
+        />
         <RaceCard selectedRace="half-elf" img={half_elf} />
         <RaceCard selectedRace="half-orc" img={half_orc} />
         <RaceCard selectedRace="human" img={human} />
         <RaceCard selectedRace="tiefling" img={tiefling} />
       </section>
-    </div>
+      <Link to="/home/abilities">
+        <BsFillArrowRightSquareFill className="arrow-button" />
+      </Link>
+    </RaceLayout>
   );
 }
+
+const RaceLayout = styled.div`
+  .arrow-button {
+    font-size: 50px;
+    color: black;
+  }
+
+  .arrow-button:hover {
+    color: red;
+  }
+`;
